b8539246edd992083a198c71ce052292f5163413
kaj <zekewotring@gmail.com>
2022-04-13 20:18:18 +0000

    feat: stuff :)

--- pages/index.js
+++ pages/index.js
@@ -33,8 +33,14 @@ export default function Home() {
               </svg>
             </a>
             <a href="https://twitter.com/KAJ718" target='_blank' rel="noreferrer">
-              <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 hover:scale-110 fill-transparent hover:fill-blue-400 hover:text-blue-400 transition duration-200 ease-in-and-out" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg xmlns="http://www.w3.org/2000/svg" className="mx-2 hover:scale-110 fill-transparent hover:fill-blue-400 hover:text-blue-400 transition duration-200 ease-in-and-out" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5 0-.28-.03-.56-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+              </svg>
+            </a>
+            <a href='mailto:kaj@otter.university'>
+              <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 hover:scale-110 fill-transparent hover:fill-blue-400 hover:text-blue-400 transition duration-200 ease-in-and-out" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                <polyline points="22,6 12,13 2,6"></polyline>
               </svg>
             </a>
           </div>
@@ -44,7 +50,7 @@ export default function Home() {
           </h2>
 
           <div className='flex flex-row '>
-            None for now...
+            Still building this out...
           </div>
 
           <h2 className='font-mono font-semibold text-[#c0c0c0] text-lg mt-5'>
@@ -52,10 +58,9 @@ export default function Home() {
           </h2>
 
           <div className='flex flex-row '>
-            None for now...
+            <img src="https://skillicons.dev/icons?i=python,cs,unity,rust,js,nodejs,mongodb,redis,git,react,next,html,css,tailwind,cloudflare,discord,docker,figma,pr,flask,linux," />
           </div>
         </div>
-
       </main>
     </div>
   )
