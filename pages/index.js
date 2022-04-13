da92762fdb7cdec73985c16f3dcaf6e1b1fef9aa
KAJ7 <zekewotring@gmail.com>
2022-04-13 15:54:15 -0800

    experience

--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,19 @@ export default function Home() {
       <main className={styles.main}>
         <div className='w-9/10 sm:w-[40em] text-left'>
 
-          <h1 className="text-6xl font-semibold">
-            Kaj
+          <h1 className="text-6xl font-bold">
+            KAJ
           </h1>
 
           <p className='font-mono mt-3'>
-            Full-stack engineer and Computer Science student.
+            Full-stack engineer and Computer Science student with a little bit of a passion for web development.
           </p>
 
           <h2 className='font-mono font-semibold text-[#c0c0c0] text-lg mt-5'>
             CONTACT
           </h2>
 
-          <div style={{display: 'flex', flexDirection: 'row', marginTop: '0.5em'}}>
+          <div style={{display: 'flex', flexDirection: 'row', marginTop: '0.25em'}}>
             <a href="https://github.com/kajdev" target='_blank' rel="noreferrer">
               <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 hover:scale-110 fill-transparent hover:fill-blue-400 hover:text-blue-400 transition duration-200 ease-in-and-out" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
@@ -49,8 +49,42 @@ export default function Home() {
             EXPERIENCE
           </h2>
 
-          <div className='flex flex-row '>
-            
+          <div className='grid grid-cols-1 gap-2'>
+            <a className='flex flex-row' href='https://blox.link' target={'_blank'} rel='referrer'>
+              <img src='/bloxlink.png' className='w-12 h-12 rounded-xl' />
+              <div className='flex flex-col ml-2'>
+                <h1 className='font-semibold text-lg'>
+                  Bloxlink
+                </h1>
+                <p className='font-mono'>
+                  Software Engineer
+                </p>
+              </div>
+            </a>
+
+            <a className='flex flex-row' href='https://safelink.gg' target={'_blank'} rel='referrer'>
+              <img src='/safelink.png' className='w-12 h-12 rounded-xl' />
+              <div className='flex flex-col ml-2'>
+                <h1 className='font-semibold text-lg'>
+                  SafeLink
+                </h1>
+                <p className='font-mono'>
+                  Founder & Maintainer
+                </p>
+              </div>
+            </a>
+
+            <a className='flex flex-row' href='https://tixte.com' target={'_blank'} rel='referrer'>
+              <img src='/tixte.png' className='w-12 h-12 rounded-xl' />
+              <div className='flex flex-col ml-2'>
+                <h1 className='font-semibold text-lg'>
+                  Tixte
+                </h1>
+                <p className='font-mono'>
+                  Founder
+                </p>
+              </div>
+            </a>
           </div>
 
           <h2 className='font-mono font-semibold text-[#c0c0c0] text-lg mt-5'>
