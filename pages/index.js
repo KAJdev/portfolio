cf6803ee65dd4f67df4c9155481715b1f36daf7e
kaj <zekewotring@gmail.com>
2022-04-13 19:41:04 +0000

    animations and tailwind

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-6xl font-semibold">
           I&apos;m Kaj!
         </h1>
 
@@ -20,14 +20,14 @@ export default function Home() {
           I&apos;ve done a couple things.
         </p>
 
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-          <a href="https://github.com/kajdev" target='_blank'>
-            <svg xmlns="http://www.w3.org/2000/svg" style={{marginRight: '1em'}} className={styles.linkItem} width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <div style={{display: 'flex', flexDirection: 'row', marginTop: '1em'}}>
+          <a href="https://github.com/kajdev" target='_blank' rel="noreferer">
+            <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 hover:scale-110 fill-transparent hover:fill-blue-400 hover:text-blue-400 transition duration-200 ease-in-and-out" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
             </svg>
           </a>
-          <a href="https://twitter.com/KAJ718" target='_blank'>
-            <svg xmlns="http://www.w3.org/2000/svg" style={{marginRight: '1em'}} className={styles.linkItem} width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <a href="https://twitter.com/KAJ718" target='_blank' rel="noreferer">
+            <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 hover:scale-110 fill-transparent hover:fill-blue-400 hover:text-blue-400 transition duration-200 ease-in-and-out" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5 0-.28-.03-.56-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
             </svg>
           </a>
